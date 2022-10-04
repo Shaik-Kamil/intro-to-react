cf9b31d9237be5b4b7744980b9b586ec6baa7463
Shaik-Kamil <107490051+Shaik-Kamil@users.noreply.github.com>
2022-10-04 12:11:46 -0400

    Tuesday lesson

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+//import packages
+//import Components
+//import
+import Main from './Main';
 import './App.css';
 
+const num1 = 3;
+const num2 = 7;
+
+// curly brackets signify Js inside JSX
+
+function sum(x, y) {
+  return x + y;
+}
+
+const name = {
+  fName: 'Shaik',
+  lName: 'Kamil',
+};
 function App() {
   return (
     <div className="App">
-      <h1>Pursuit App Template</h1>
+      <h1 style={{ color: 'blue', backgroundColor: 'yellow' }}>
+        Pursuit App Template
+      </h1>
+      {name.fName} {name.lName}
+      <p style={{ border: '1px solid black', height: '100px' }}>
+        {num1 + num2}
+      </p>
+      <p> {sum(num1, num2)}</p>
+      <Main />
     </div>
   );
 }
---
+++ src/Main.js
@@ -0,0 +1,11 @@
+import './Main.css';
+
+const Main = () => {
+  return (
+    <div>
+      <h1 className="Mainh1">Main Component</h1>
+    </div>
+  );
+};
+
+export default Main;
