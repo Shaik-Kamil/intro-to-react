236340b91e69bca7bb74f4a047d7c6a729b4f9dc
Shaik-Kamil <107490051+Shaik-Kamil@users.noreply.github.com>
2022-10-05 11:43:04 -0400

    passing all

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import Header from './Header';
 import Footer from './Footer';
 import './App.css';
 
+//!   <></> is a fragment which we can use if we don't want to add an element to an tree,
+//!   className or id can't be used in fragments <></>
+
 const App = () => {
   return (
     <div style={{ border: '2px solid red' }}>
